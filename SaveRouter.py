59855f07edc45130f75379e40f17630a9d5eb6b8
Nick K <ivankapalala@gmail.com>
2017-10-26 17:09:43 +0200

    Create SaveRouter.py

---
+++ SaveRouter.py
@@ -0,0 +1,23 @@
+import getpass
+import sys
+import telnetlib
+
+
+HOST = "" #insert host address
+user = raw_input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("enable\n")
+tn.write("test\n")
+tn.write("copy running-config startup-config\n")
+tn.read_until("Destination filename [startup-config]? ")
+tn.write("\n")
+tn.write("exit\n")
